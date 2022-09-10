2b0112b8aba606fa790ab753c6b7329dfbef5c18
mifouts <fouts.mitch15@gmail.com>
2022-09-10 15:51:58 -0400

    cleaning up the code

--- user.js
+++ user.js
@@ -1,37 +1,31 @@
 console.log();
 
 const postListEl = document.querySelector('.post-list');
+const id = localStorage.getItem("id");
+
+
 
 async function onSearchChange(event) {
     const id = event.target.value;
+    renderPosts(id)
+}
+
+async function renderPosts(userID) {
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
     const postsData = await posts.json();
-    postListEl.innerHTML = postsData.map(post =>
-    `<div class="post" onclick="showUserPosts(${post})">
+    postListEl.innerHTML = postsData.map(post => postsHTML(post)).join('');
+}
+
+function postsHTML(post){
+    return   `<div class="post">
     <div class="post__title">
         ${post.title}
     </div>
     <p class="post__body">
       ${post.body}
     </p>
-  </div>`).join('');
-}
-
-async function main() {
-    const id = localStorage.getItem("id");
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-    const postsData = await posts.json();
-
-    postListEl.innerHTML = postsData.map(post =>
-    `<div class="post">
-    <div class="post__title">
-        ${post.title}
-    </div>
-    <p class="post__body">
-      ${post.body}
-    </p>
-  </div>`).join('');
+  </div>`
 }
 
 
-main();
+renderPosts();
