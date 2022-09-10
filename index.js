6d987f6e5031f6124f8b6edfa26b90fb5b5b67b7
mifouts <fouts.mitch15@gmail.com>
2022-09-10 14:39:53 -0400

    making it dynamic

--- index.js
+++ index.js
@@ -4,18 +4,21 @@
 async function main () {
     const users = await fetch('https://jsonplaceholder.typicode.com/users')
     const usersData = await users.json();
-    console.log(
-    usersData.map(
+    const userListEl = document.querySelector(".user-list");
+    userListEl.innerHTML = usersData
+        .map(
         (user) => `<div class="user-card">
         <div class="user-card__container">
-          <h3>User's Name</h4>
-            <p><b>Email:</b> email@email.com</p>
-            <p><b>Phone:</b> 0000000000</p>
-            <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+          <h3>${user.name}</h4>
+            <p><b>Email:</b> ${user.email}</p>
+            <p><b>Phone:</b> ${user.phone}</p>
+            <><b>Website:</b> < href="https://${user.website}" target="_blank">
+            ${user.website}
+            </a></p>
         </div>
       </div>`
       )
-    );
+    .join("");
 }
 
 main();
