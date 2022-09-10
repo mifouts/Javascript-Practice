2d27bec3b2cd417ce7bf9a77df482224f85374bf
mifouts <fouts.mitch15@gmail.com>
2022-09-10 14:48:50 -0400

    re routing to new page

--- index.js
+++ index.js
@@ -5,20 +5,25 @@ async function main () {
     const users = await fetch('https://jsonplaceholder.typicode.com/users')
     const usersData = await users.json();
     const userListEl = document.querySelector(".user-list");
-    userListEl.innerHTML = usersData
-        .map(
-        (user) => `<div class="user-card">
-        <div class="user-card__container">
-          <h3>${user.name}</h4>
-            <p><b>Email:</b> ${user.email}</p>
-            <p><b>Phone:</b> ${user.phone}</p>
-            <><b>Website:</b> < href="https://${user.website}" target="_blank">
-            ${user.website}
-            </a></p>
-        </div>
-      </div>`
-      )
-    .join("");
+    userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
 }
 
 main();
+
+function showUserPosts(id) {
+    window.location.href = 'http://127.0.0.1:5500/user.html'
+}
+
+
+function userHTML(user) {
+    return `<div class="user-card" onclick="showUserPosts(${user.id})">
+    <div class="user-card__container">
+      <h3>${user.name}</h4>
+        <p><b>Email:</b> ${user.email}</p>
+        <p><b>Phone:</b> ${user.phone}</p>
+        <><b>Website:</b> < href="https://${user.website}" target="_blank">
+        ${user.website}
+        </a></p>
+    </div>
+  </div>`;
+}
