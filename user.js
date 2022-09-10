3dc0e6c198226f2d4fea192945a0e90f262fe397
mifouts <fouts.mitch15@gmail.com>
2022-09-10 15:53:40 -0400

    making code more readable

--- user.js
+++ user.js
@@ -10,7 +10,7 @@ async function onSearchChange(event) {
     renderPosts(id)
 }
 
-async function renderPosts(userID) {
+async function renderPosts(id) {
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
     const postsData = await posts.json();
     postListEl.innerHTML = postsData.map(post => postsHTML(post)).join('');
